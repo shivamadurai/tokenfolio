342cd94597f4fa0bce8ff445896992d8e5c4d697
shivamadurai <shivamadurai@gmail.com>
2024-12-07 19:27:28 +0530

    Layout fix

--- src/components/GetCrypto/index.tsx
+++ src/components/GetCrypto/index.tsx
@@ -35,72 +35,75 @@ export const GetCrypto: FC = (): JSX.Element => {
     // console.log(data, error, isLoading);
     return (
         <div className="crypto-page">
-            <Row className="search-header">
-                <Col className="box-header" xs={24} md={24} xl={24}>
-                    <h3 className="text-label">Search Currency</h3>
-                    <div className="search-box">
-                        <Input
-                            allowClear
-                            className="search-code"
-                            placeholder="Enter Currency Code"
-                            onChange={(e) => {
-                                const code: string = e?.target?.value;
-                                setText(code);
-                                if (code.length >= 3) {
-                                    setCurrency(code);
-                                }
-                            }}
-                            value={text}
-                        />
-                        <Select
-                            placeholder={"Currency Code"}
-                            aria-label="Select Currency"
-                            className="currency-select"
-                            onChange={(e: string) => {
-                                clearText();
-                                setCurrency(e);
-                            }}
-                            value={currency}
-                            options={codeHistory.map((item) => ({ value: item, label: item }))}
-                        ></Select>
-                    </div>
-                </Col>
-            </Row>
-
-            {isLoading && <p>Loading...</p>}
-            {!isLoading && error && <p>Error: {error}</p>}
-            {!isLoading && data && data.length > 0 ? (
-                <ul className="crypto-list">
-                    <Row>
-                        {data.map((item: any) => (
-                            <Col key={item?.id} xs={24} md={12} xl={6}>
-                                <li>
-                                    <img width={80} height={80} src={item?.image} alt={item.symbol} />
-                                    <div className="content">
-                                        <div className="name" title={item?.name}>
-                                            Name: {item?.name}
+            <div className="header-section">
+                <Row className="search-header">
+                    <Col className="box-header" xs={24} md={24} xl={24}>
+                        <h3 className="text-label">Search Currency</h3>
+                        <div className="search-box">
+                            <Input
+                                allowClear
+                                className="search-code"
+                                placeholder="Enter Currency Code"
+                                onChange={(e) => {
+                                    const code: string = e?.target?.value;
+                                    setText(code);
+                                    if (code.length >= 3) {
+                                        setCurrency(code);
+                                    }
+                                }}
+                                value={text}
+                            />
+                            <Select
+                                placeholder={"Currency Code"}
+                                aria-label="Select Currency"
+                                className="currency-select"
+                                onChange={(e: string) => {
+                                    clearText();
+                                    setCurrency(e);
+                                }}
+                                value={currency}
+                                options={codeHistory.map((item) => ({ value: item, label: item }))}
+                            ></Select>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <div className="body-section">
+                {isLoading && <p>Loading...</p>}
+                {!isLoading && error && <p>Error: {error}</p>}
+                {!isLoading && data && data.length > 0 ? (
+                    <ul className="crypto-list">
+                        <Row>
+                            {data.map((item: any) => (
+                                <Col key={item?.id} xs={24} md={12} xl={6}>
+                                    <li>
+                                        <img width={80} height={80} src={item?.image} alt={item.symbol} />
+                                        <div className="content">
+                                            <div className="name" title={item?.name}>
+                                                Name: {item?.name}
+                                            </div>
+                                            <div>Price: {item?.current_price}</div>
+                                            <div>Currency: {currency}</div>
+                                            <div>
+                                                <a
+                                                    onClick={(e) => {
+                                                        setModalItem(item);
+                                                        setModal(true);
+                                                    }}
+                                                >
+                                                    View More
+                                                </a>
+                                            </div>
                                         </div>
-                                        <div>Price: {item?.current_price}</div>
-                                        <div>Currency: {currency}</div>
-                                        <div>
-                                            <a
-                                                onClick={(e) => {
-                                                    setModalItem(item);
-                                                    setModal(true);
-                                                }}
-                                            >
-                                                View More
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </Col>
-                        ))}
-                    </Row>
-                </ul>
-            ) : (
-                <p>Error: {JSON.stringify(data)}</p>
-            )}
+                                    </li>
+                                </Col>
+                            ))}
+                        </Row>
+                    </ul>
+                ) : (
+                    data && <p>Error: {JSON.stringify(data?.error || data?.status?.error_message)}</p>
+                )}
+            </div>
             {showModal && <Modal item={modalItem} currency={currency} onBackdropClick={closeModal} />}
         </div>
     );
